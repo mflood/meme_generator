75ed80a4a22456b6b0d80c3969de1d99e0a39f52
mflood <hermannwest@gmail.com>
2024-06-28 10:16:22 -0400

    Add magic methods to QuoteModel

--- models.py
+++ models.py
@@ -7,5 +7,22 @@ class QuoteModel:
     """
     Represents a quote
     """
-    author: str
     body: str
+    author: str
+
+    def __str__(self):
+        return f'"{ self.body }" - {self.author}'
+
+    def __eq__(self, other):
+        if isinstance(other, QuoteModel):
+            return (self.body.lower() == other.body.lower() and
+                    self.author.lower() == other.author.lower())
+        return False
+
+    def __hash__(self):
+        return hash((self.body.lower(), self.author.lower()))
+
+if __name__ == "__main__":
+
+    qm = QuoteModel(body="I'm ready", author="Stephen King")
+    print(qm)
