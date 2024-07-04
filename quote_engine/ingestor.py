bd7d3950cf4727c2ea9adbf473e9995867622df3
mflood <hermannwest@gmail.com>
2024-07-04 13:49:21 -0400

    Add custom UnsupportedFileTypeError

--- quote_engine/ingestor.py
+++ quote_engine/ingestor.py
@@ -1,36 +1,41 @@
-
-import os
-
+from pathlib import Path
 from typing import List
-from quote_engine.models import QuoteModel
 
-from quote_engine.ingestor_interface import IngestorInterface
-from quote_engine.txt_ingestor import TxtIngestor
 from quote_engine.csv_ingestor import CsvIngestor
-from quote_engine.pdf_ingestor import PdfIngestor
 from quote_engine.docx_ingestor import DocxIngestor
+from quote_engine.ingestor_interface import IngestorInterface
+from quote_engine.models import QuoteModel
+from quote_engine.pdf_ingestor import PdfIngestor
+from quote_engine.txt_ingestor import TxtIngestor
+
+
+class UnsupportedFileTypeError(ValueError):
+    """Exception raised for unsupported file types."""
+
 
 class Ingestor(IngestorInterface):
 
     ingestors = [TxtIngestor, CsvIngestor, PdfIngestor, DocxIngestor]
 
-    @classmethod    
+    @classmethod
     def can_ingest(cls, path: str) -> bool:
         for ingestor in Ingestor.ingestors:
             if ingestor.can_ingest(path):
                 return True
         return False
-        
+
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
         for ingestor in Ingestor.ingestors:
             if ingestor.can_ingest(path):
                 print(f"using {ingestor} to parse {path}")
                 return ingestor.parse(path)
-        
-        raise Exception()
-        return [] 
- 
+
+        file_type = Path(path).suffix[1:]
+        name = Path(path).name
+        message = f"Could not parse '{name}'. Unsupported filetype: {file_type}."
+        raise UnsupportedFileTypeError(message)
+
     @classmethod
     def parse_files(cls, path_list: List[str]) -> List[QuoteModel]:
         quotes: List[QuoteModel] = []
@@ -42,7 +47,6 @@ class Ingestor(IngestorInterface):
         return unique_quotes
 
 
-
 if __name__ == "__main__":
 
     print(Ingestor.can_ingest("myfile.txt"))
@@ -50,3 +54,4 @@ if __name__ == "__main__":
     print(Ingestor.can_ingest("myfile.pdf"))
     print(Ingestor.can_ingest("myfile.csv"))
     print(Ingestor.can_ingest("myfile.bmp"))
+    Ingestor.parse(path="myfile.bmp")
